50ea728234c3b17ef8fa6e5fa41a82799d25b828
Dieter Halosta <dieter.halosta@yahoo.com>
2020-02-24 20:47:55 +0200

    studied classes, variables, primitive data types

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -8,6 +8,31 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Welcome to the racing game!" );
+
+        Car carReference = new Car();
+        carReference.name = "Audi";
+        carReference.color = "red";
+        carReference.maxSpeed = 260;
+        carReference.mileage = 6.2;
+
+        // concatenation
+        System.out.println("Properties of " + carReference.name + ":");
+        System.out.println("Color: " + carReference.color);
+        System.out.println("Max Speed: " + carReference.maxSpeed);
+        System.out.println("Mileage: " + carReference.mileage);
+        System.out.println("Fuel level: " + carReference.fuelLevel);
+        System.out.println("Racing number: " + carReference.racingNumber);
+        System.out.println("Damage: " + carReference.damaged);
+        System.out.println("--------------------------------------------------");
+
+        Car car2 = new Car();
+        car2.name = "Lamborghini";
+        car2.damaged = true;
+
+        System.out.println("Properties of " + car2.name + ":");
+        System.out.println("Color: " + car2.color);
+        System.out.println("Damage: " + car2.damaged);
     }
+
 }
