13362fbe83c70216cd51dbcb103060b1777c425d
Dieter Halosta <dieter.halosta@yahoo.com>
2020-03-09 20:26:52 +0200

    Studied Methods

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -17,6 +17,7 @@ public class App
         carReference.color = "red";
         carReference.maxSpeed = 260;
         carReference.mileage = 6.2;
+        carReference.fuelLevel = 100;
 
   //     carReference.engine = new Engine();
   //     carReference.engine.manufacturer = "VW";
@@ -27,6 +28,11 @@ public class App
         engine1.capacity = 2000;
 
         carReference.engine = engine1;
+
+       double currentDistance = carReference.accelerate(60, 1);
+
+        System.out.println("Current distance: " + currentDistance);
+
 
         // One object with two references
 
--- src/main/java/org/fasttrackit/Vehicle.java
+++ src/main/java/org/fasttrackit/Vehicle.java
@@ -8,5 +8,29 @@ public class Vehicle {
     double mileage;
     String color;
     double fuelLevel;
+    double totalDistance;
     boolean damaged;
+
+
+    // Method signature
+    public double accelerate(double speed, double durationInHours) {
+        // Method body
+        System.out.println(name + " is accelerating with " + speed + " km/h for" + durationInHours + " h");
+
+        // local variables - variable declarate in interiorul unei metode si este doar pentru metoda respectiva
+        double traveledDistance = speed * durationInHours;
+        System.out.println("Traveled distance: " + traveledDistance);
+
+        totalDistance += traveledDistance;
+        System.out.println("Total traveled distance: " + totalDistance);
+
+
+
+        double usedFuel = traveledDistance * mileage / 100;
+
+        fuelLevel -= usedFuel; // same as fuelLevel = fuelLevel - usedFuel
+        System.out.println("Remaining fuel: " + fuelLevel);
+
+        return traveledDistance;
+    };
 }
