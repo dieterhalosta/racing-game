e6fda4fe00dd2764ae9255e246b7f4c541ebe7a0
Dieter Halosta <dieter.halosta@yahoo.com>
2020-03-09 19:18:13 +0200

    Studied Inheritance

--- src/main/java/org/fasttrackit/Car.java
+++ src/main/java/org/fasttrackit/Car.java
@@ -1,15 +1,6 @@
 package org.fasttrackit;
 
-public class Car {
-    // instance variables
-    int racingNumber;
-    String name;
-    int maxSpeed;
-    double mileage;
-    String color;
-    double fuelLevel;
-    boolean damaged;
-    // "has-a" relationship / Aggregation
-    Engine engine;
-
+// "is-a" relationship or Inheritance
+public class Car extends AutoVehicle{
+    int doorCount;
 }
